48260ef8a4ca53a20b389ece26df361927c76d2f
Irma Fraticelli <irma.fraticelli@yale.edu>
2025-03-24 11:44:22 -0400

    Generate dataset report   - includes fields 001, 090, 245, 264, 300 and 856 for bibligraphic datasets, and   - related holdings that may have field 856.

--- marc/marc_helpers.py
+++ marc/marc_helpers.py
@@ -1,10 +1,12 @@
-def is_holding(record):
+from pymarc.record import Record
+
+def is_holding(record: Record):
     return record.leader[6] in 'uvxy'
 
-def is_authority(record):
+def is_authority(record: Record):
     return record.leader[6] in 'z'
 
-def record_type(record):
+def record_type(record: Record):
     if record.leader[6] in 'z':
         return 'authority'
     if record.leader[6] in 'uvxy':
@@ -15,11 +17,41 @@ def record_type(record):
         return 'community'
     return 'bibliographic'
 
-def record_id(record):
+def record_id(record: Record):
     return record['001'].value()
 
-def extract_values(record, field_tag, subfield_tags):
+def extract_values(record: Record, field_tag, subfield_tags):
     values = []
     for field in record.get_fields(field_tag):
         values += field.get_subfields(*subfield_tags)
     return values
+
+def record_fields_to_dict(
+        record: Record, 
+        fields: list[str], 
+        repeatable_fields: list[str]) -> dict[str,str|list[str]]:
+    """Returns a dictionary with record field values by tag. Multi 
+    valued fields are returned as a list of strings. If a repeatable is
+    not correctly identified, only the last instance of that field process
+    will be preserved.
+
+    Args:
+        record (Record): A MARC record instance.
+        fields (list[str]): List of field tags that we want to extract.
+        repeatable_fields (list[str]): List of repeatable fields to be extracted.
+
+    Returns:
+        dict[str,str|list[str]]: Dictionary mapping requested field tags to value or list of values obtained.
+    """
+    record_dict = dict()
+    record_dict["leader"] = list(record.leader)
+    fields = record.get_fields(*fields)
+    for field in fields:
+        if field.tag not in repeatable_fields:
+            assert field.tag not in record_dict.keys(), f"Tag '{field.tag}' found multiple times for `{record.leader}`. You may want to include it in your `repeatable_fields` list."
+            record_dict[field.tag] = field.value()
+        else:
+            if field.tag not in record_dict.keys():
+                record_dict[field.tag]=[]
+            record_dict[field.tag].append(field.value())
+    return record_dict
---
+++ processors/dataset_report.py
@@ -0,0 +1,74 @@
+from base_processor import BaseProcessor
+from marc.marc_helpers import is_holding, Record, record_type
+from sys import stderr
+import re
+
+def extract_record_fields(record: Record, fields: list[str], repeatable_fields: list[str]):
+        """This function is a utility to extract MARC fields from a Record object and return a dictionary"""
+        record_dict = dict()
+        fields = record.get_fields(*fields)
+        for field in fields:
+            if field.tag not in repeatable_fields:
+                record_dict[field.tag] = field.value()
+            else:
+                if field.tag not in record_dict.keys():
+                    record_dict[field.tag]=[]
+                record_dict[field.tag].append(field.value())
+        return record_dict
+
+
+class DataSetReport(BaseProcessor):
+    def __init__(self, reporter):
+        self.reporter = reporter
+        self.dataset_bibids = set()
+        print("Initiated DataSetReport Processor", file=stderr)
+
+    def is_dataset_holding(self, record: Record):
+        return record.get_fields("004")[0].value() in self.dataset_bibids
+
+    @staticmethod
+    def is_dataset_record(record: Record):
+        is_dataset = False
+        for f090 in record.get_fields("090"):
+            # if "yuldset" in f090.value():
+            # if "yuldset" in f090.value():
+            if re.match(r"yuldset", f090.value()):
+                is_dataset = True
+                break
+        return is_dataset
+    
+    def marc_record(self, record: Record):
+        """Generate report that includes fields
+        001, 090, 245, 264, 300 and 856 for bibligraphic
+        datasets and related holdings that may have 856.
+
+        Args:
+            record: Record: _description_
+        """
+        fields = [ "001", "090", "245", "264", "300","856" ]
+        hold_fields = [ "001", "004", "852", "856"]
+        repeatable_fields = ["090", "264", "300", "852", "856"]
+        if is_holding(record):
+            # Holding Record Processing
+            try:
+                if self.is_dataset_holding(record):
+                    hold_dict = extract_record_fields(record, hold_fields, repeatable_fields)
+                    print(hold_dict)
+            except IndexError:
+                hold_dict = extract_record_fields(record, ["001","090","245","264","300","004","852","856"], repeatable_fields)
+                known_error = False
+                for v852 in hold_dict.get("852", []):
+                    if "circBDIR" in v852:
+                        known_error = True
+                if not known_error:
+                    print(hold_dict, file=stderr)
+        else:
+            # Bibligraphic Record Processing
+            if DataSetReport.is_dataset_record(record):
+                    bib_dict = extract_record_fields(record,fields,repeatable_fields)
+                    if "001" in bib_dict.keys():
+                        self.dataset_bibids.add(bib_dict["001"])
+                    else:
+                        print(f"WARNING Missing 001: {bib_dict}", file=stderr)
+                    print(bib_dict)
+
